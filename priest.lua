00bb4e7f450aaa3bb8fea8f4927db345a420d794
faelz <faelz@live.com>
2018-12-06 22:22:11 -0200

    priest bot updated

--- priest.lua
+++ priest.lua
@@ -1,8 +1,5 @@
 g_PriestBot = nil
 
-local BOTSTATE_NONE    = 0
-local BOTSTATE_HEALING = 1
-
 class 'CPriestBot'
 function CPriestBot:__init(app)
   self.app = app
@@ -17,13 +14,36 @@ function CPriestBot:__init(app)
   self.objects = { {} }
   self.enabled = false
   self.state = 1
-  self.botstate = BOTSTATE_NONE
   self.time_lastheal = 0
   self.time_bufmanager = 0
   self.time_queue = 0
   self.queuedkeys = { }
   
-  print("CPriestBot::Construct()")
+  print("CPriestBot()") --debug
+end
+
+function CPriestBot:CelestialLight()
+  self:QueueKeyEvent(0x2D)  -- X
+end
+
+function CPriestBot:HolyBlast()
+  self:QueueKeyEvent(0x2C) -- Z
+end
+
+function CPriestBot:HealingPreyer()
+  self:QueueKeyEvent(0x2F) -- V
+end
+
+function CPriestBot:ShieldOfArchon()
+  self:QueueKeyEvent(0x12) -- E
+end
+
+function CPriestBot:CelestialGuardian()
+  self:QueueKeyEvent(0x10) -- Q
+end
+
+function CPriestBot:HolyRelic()
+  self:QueueKeyEvent(0x14) -- T
 end
 
 function CPriestBot:GetClientTime()
@@ -31,12 +51,11 @@ function CPriestBot:GetClientTime()
 end
 
 function CPriestBot:QueueKeyEvent(key)
-  local t = {
-    vk = key,
-	state = 0,
-	t = self:GetClientTime()
-  }
-  self.queuedkeys[#self.queuedkeys+1] = t  
+  local t = { vk = key, 
+              state = 0, -- 0 awaiting, 1 = down, 2 = up (finished)
+			  t = self:GetClientTime() 
+			}
+  self.queuedkeys[#self.queuedkeys+1] = t
 end
 
 function CPriestBot:KeyPressManager()
@@ -59,15 +78,38 @@ function CPriestBot:KeyPressManager()
   end 
 end
 
-function CPriestBot:HealingManager(target)
-  if not target then
-    return
-  end
+function CPriestBot:AttackManager(target)
+   local dist = self:GetDistance(target)
+	
+   --if dist > 9 and self.myPC:GetEstamine() > 35  then
+	--self:QueueKeyEvent(0x2E) -- C celestial wings
+   --else
+	
+   local pos = target:GetPos()
+   self:DrawArea(self.camera, 1*100, clRed, pos) -- 1m radius
   
-  local pos = target:GetPos()
+   if dist > 2 then
+     local out = Vector3(0,0,0)
+	 if self.camera:WorldToScreen(target:GetPos(), out) then
+		local screen = Vector2(out.x,out.y)
+		self.game:Move2D(screen)
+	 end
+   end
+	
+   local _floor = math.abs(pos.z - self.myPC:GetPos().z)/100
   
-  self:DrawArea(self.camera, 80, clRed, pos)
+   if _floor < 1 then -- 1m above or below of local pc
+     if dist < 4 and spirit > 45 then -- 4m is the holy blast range
+         self:HolyBlast()
+	 elseif dist < 8 then
+	   self:CelestialLight()
+	 end
+   end
+end
 
+function CPriestBot:HealingManager()
+  local healing = true
+  
   local t = self:GetClientTime() - self.time_lastheal
   if t < 400 then
     return
@@ -76,52 +118,25 @@ function CPriestBot:HealingManager(target)
   local health = self.myPC:GetHealth()
   local maxHealth = self.myPC:GetMaxHealth()
   local spirit = self.myPC:GetSpirit()
-  
-  local relevo = math.abs(pos.z - self.myPC:GetPos().z)/100
-  
+    
   if health < maxHealth/2 + maxHealth/4 then
-    self:QueueKeyEvent(0x2F) --V heal 
+    self:HealingPreyer()
   else
-    local dist = self:GetDistance(target)
-	
-	--if dist > 9 and self.myPC:GetEstamine() > 15  then
-	  --self:QueueKeyEvent(0x2E) -- C flash jump
-    --else
-	if dist > 2 then
-	  local out = Vector3(0,0,0)
-	  if self.camera:WorldToScreen(pos, out) then
-		local screen = Vector2(out.x,out.y)
-		self.game:Move2D(screen)
-	  end
-	end
-  
-    if relevo < 1 then    
-      if dist < 4 and spirit > 45 then
-         self:QueueKeyEvent(0x2C) -- Z holy blast
-	   else
-	     if dist < 8 then
-	       self:QueueKeyEvent(0x2D)  -- X celestial light
-		 end
-       end
-    end
+    healing = false
   end
-      
   self.time_lastheal = self:GetClientTime()
+  
+  return healing
 end
 
-function CPriestBot:BufManager(target)
-  if not target then
-    return
-  end
-
+function CPriestBot:BufManager()
   local t = self:GetClientTime() - self.time_bufmanager
-  if t < 1000*10 then
+  if t < 1000*10 then --every 10s
     return
   end
-  self:QueueKeyEvent(0x12) -- E sheield of archon
-  self:QueueKeyEvent(0x10) -- Q celestial guardian
-  self:QueueKeyEvent(0x14) -- T holy relic
-  --print("bufs activated")
+  self:ShieldOfArchon()
+  self:CelestialGuardian()
+  self:HolyRelic()
   self.time_bufmanager = self:GetClientTime()
 end
 
@@ -167,202 +182,108 @@ function CPriestBot:GetDistance(obj)
   return 0
 end
 
-function CPriestBot:FindClosest(objs, obj)
-  local result = nil
-  local olddist = 999    
-  for i = 1, #objs do
-	local pc = objs[i]
-	local dist = self:GetDistance(pc)
-	local oid = pc:GetOID()
-	
-	if oid ~= self.myPC:GetOID() then
-	  if oid ~= obj:GetOID() and dist < olddist then
-	    olddist = dist
-	    result = pc
-	  end
-    end	  
-  end
-  return result
-end
-
-function CPriestBot:IsInList(list, obj)
-  for i = 1, #list do
-    if obj:GetOID() == list[i]:GetOID() then
-	  return true
-	end
-  end
-  return false
-end
-
-function CPriestBot:LoadObjectList()  
-  --[[local objs = self.game:GetObjects()
-  self.objects = {}
-  for i = 1, #objs do
-	local pc = objs[i]
-    local closest = self:FindClosest(objs, pc)
-	if closest then 
-	  if not self:IsInList(self.objects, closest) then
-	    self.objects[#self.objects + 1] = closest
-	  end
-	end
-  end
-  --]]
-  self.objects = self.game:GetObjects()
-end
-
-function CPriestBot:UseSkill(state)
-  local t = self.game:GetMainSystem():GetClientTime() - self.skilltime
-  
-  
-  
-  if t > 200 then
-    
-	--g_App:KeyDown(0x2D)
-    --g_App:KeyUp(0x2D)
-		
-	self.skilltime = self.game:GetMainSystem():GetClientTime()
-  end
-end
-
-function CPriestBot:checkName(pc)
-  local name = pc:GetNameA()
-  return 
-    string.find(name, "Gyatus") or 
-	string.find(name, "Cursed") or 
-	string.find(name, "Drake")
-end
-
-function CPriestBot:ShowObjectList()
+function CPriestBot:ListObjects()
   local x = 10
   local y = 100 
   local olddist = 999
   local target = nil
+  local color = clRed
+  
+  if self.enabled then
+    color = clGreen
+  end
 
+  self.fontObjectList:Draw(x,y,0,0, color, "[F1] Bot disabled")
+  y = y + 10
   self.fontObjectList:Draw(x,y,0,0, clYellow, string.format("count %d", #self.objects))
   
-  --[[
-  local closest = self:FindClosest(#self.objects, self.myPC)
-  if closest then
-    self:DrawArea(self.game:GetCamera(), 10, closest:GetPos())
-  end--]]
-  
-  --self:DrawArea(self.game:GetCamera(), 10, closest:GetPos())
-  
-
-  local out = Vector3(0,0,0)  
-  local selfPos = self.myPC:GetPos()
-  local state = self.state
-    
-  if self.camera:WorldToScreen(selfPos, out) then
-    local estamine = self.myPC:GetEstamine()
-    self.fontObjectList:Draw(out.x,out.y,0,0, clYellow, string.format("%d %d", state, estamine))
-	self:DrawArea(self.camera, 8*100, clYellow, selfPos)
-  end
+  local out = Vector3(0,0,0)
       
   for i = 1, #self.objects do
    
     local pc = self.objects[i]
 	local oid = pc:GetOID()
-    	
-	if pc:GetType() ~= 2 and oid > 0x10000 and pc:GetHealth() > 0 and pc:GetMaxHealth() > 150000.0 then
-	  if oid ~= self.myPC:GetOID() then	    
+     
+	if pc:GetType() ~= 2 and  --drops are type 2 then we ignore them
+	   oid > 0x10000 and  --npcs oids are usually < 0x10000, not a safe way but works
+	   pc:GetHealth() > 0 and --ignore if dead
+	   pc:GetMaxHealth() > 150000.0 then --this check is for identifying targets in lulu village map, also, without this pets and players are also listed (an uid will be added soon)	   
+	   if oid ~= self.myPC:GetOID() then	    
 	    
-		local dist = self:GetDistance(pc)
-	    local pos = pc:GetPos()
-		local out = Vector3(0,0,0)
-		y = y + 10
-        self.fontObjectList:Draw(x,y,0,0, clYellow, string.format("%02.0f M", dist))
-		
-		if self.camera:WorldToScreen(pos, out) then
-		  --local relevo = math.abs(target:GetPos().z - self.myPC:GetPos().z)/100
-		  
-		  
-		  
-		  self.fontObjectList:Draw(out.x,out.y,0,0, clGreen, string.format("%1.0fM", dist))
-        end
-		
-	    if dist < olddist then
-	      olddist = dist
-		  target = pc
-	    end		
-	  end
+		 local dist = self:GetDistance(pc)
+	     local pos = pc:GetPos()
+		 		 
+		 y = y + 10
+         self.fontObjectList:Draw(x,y,0,0, clYellow, string.format("%02.0f M %s", dist, pc:GetNameA()))
+		 
+		 if self.camera:WorldToScreen(pos, out) then		  
+		   self.fontObjectList:Draw(out.x,out.y,0,0, clGreen, string.format("%1.0fM", dist))
+         end
+		 
+	     if dist < olddist then
+	       olddist = dist
+		   target = pc
+	     end
+	   end
 	end
   end  
   
-  if self.camera:WorldToScreen(target:GetPos(), out) then
-    return target
+  -- check if our target is visible on screen
+  if target and not self.camera:WorldToScreen(target:GetPos(), out) then
+    target = nil
   end
   
-  
-  --self.fontObjectList:Draw(x,target_y,0,0, clRed, string.format("%1.0fm << target", olddist))
-  
-  --[[
-  local dist = self:GetDistance(target)
-  
-  if dist < 4 then
-    local out = Vector3(0,0,0)  
-	local pos = target:GetPos()
-    if camera:WorldToScreen(pos, out) then
-      self.fontObjectList:Draw(out.x,out.y,0,0, clYellow, string.format("%1.0fm", dist))
-	  self:UseSkill()
-	end
-  end
-  --]]
-end
-
-function CPriestBot:ShowMessage(text)
-  
-  self.fontDesc:Draw(100,200,0,0, clGreen, string.format("MapID - %u", self.game:GetCurrentMapID()))
+  return target
 end
 
 function CPriestBot:OnUpdate()
-  self.fontDesc:Draw(100,200,0,0, clGreen, string.format("MapID - %u", self.game:GetCurrentMapID()))
-  
-  if not self.enabled then
-    return
-  end
+  --self.fontDesc:Draw(100,200,0,0, clGreen, string.format("MapID - %u", self.game:GetCurrentMapID()))
   
   self.myPC = self.game:GetMyPC()
   if not self.myPC then
     return
   end
-  --[[
-  if not (self.myPC:GetHealth() > 0) then
-    return
-  end
-  --]]
   
   self.state = self.myPC:GetSubState()
   self.camera = self.game:GetCamera()
-  
-  self.myPC:SetAttackSpeed(100)
-  self.myPC:SetSpeed(200)
-  self.myPC:SetJump(130)
 	
-  self:LoadObjectList()
-  local target = self:ShowObjectList()
-  self:HealingManager(target)
-  self:BufManager(target)
-  self:KeyPressManager()
+  self.objects = self.game:GetObjects()
+  local target = self:ListObjects()
+  
+  if not self:HealingManager() then
+  
+	if not self.enabled then
+      return
+    end
+	
+	local out = Vector3(0,0,0)  
+    local pos = self.myPC:GetPos()
+    	
+    if self.camera:WorldToScreen(pos, out) then
+      self.fontObjectList:Draw(out.x,out.y,0,0, clYellow, string.format("%d", self.state))
+	  self:DrawArea(self.camera, 8*100, clYellow, pos) --8m radius
+    end
+	
+    if target then
+      self:AttackManager(target)
+	  self:BufManager()
+	end
+  end
+  self:KeyPressManager()  
 end
 
 function CPriestBot:WindowProc(msg)
    if msg.message == WM_KEYUP then
     if msg.wParam == VK_F2 then
-      self.enabled = not self.enabled
-	 
-	  self.app:KeyUp(0x2C) -- Z holy blast
-	  self.app:KeyUp(0x2F) --V heal 
-      self.app:KeyUp(0x2D)  -- X celestial light
+      self.enabled = not self.enabled	
 	end
   elseif msg.message == WM_MOUSEMOVE then
-    self.mouse.x = tonumber(LOWORD(msg.lParam))
-	self.mouse.y = tonumber(HIWORD(msg.lParam))
+    self.mouse.x = LOWORD(msg.lParam)
+	self.mouse.y = HIWORD(msg.lParam)
   end	
 end
 
 --------------- // -------------
-
 
 function PriestBot_OnUpdate()
   g_PriestBot:OnUpdate()
